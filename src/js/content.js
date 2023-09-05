e945dd9885a42efbb9718f53337b5da6f84ce1e0
Fehérvári Zsolt <f.zsolt@niarz.com>
2023-09-05 20:18:06 +0200

    update

--- src/js/content.js
+++ src/js/content.js
@@ -2,32 +2,26 @@ window.addEventListener("load",()=>{
 
     setTimeout(() => {
         
-        let passwordFileds = [];
+        let pwdFields = [];
 
         if('querySelectorAll' in document){
 
-            passwordFileds = document.querySelectorAll("input[type='password']");
+            pwdFields = document.querySelectorAll("input[type='password']");
 
         }else{
 
-            let inputs = document.getElementsByTagName("input");
+            const inputs = document.getElementsByTagName("input");
 
-            for (let i=0; i<inputs.length; i++) {
-                if (inputs[i].type.toLowerCase() === "password") {
-                    passwordFileds.push(inputs[i]);
-                }
+            for (let i = 0; i < inputs.length; i++) {
+                if (inputs[i].type.toLowerCase() === "password") pwdFields.push(inputs[i]);
             }
 
         }
-        
-        passwordFileds.forEach((input)=>{
 
-            input.addEventListener("keydown", function(event) {
-                
-                if ((event.ctrlKey || event.metaKey) && event.key === "c") {
-                    navigator.clipboard.writeText(input.value);
-                }
+        pwdFields.forEach((input)=>{
 
+            input.addEventListener("keydown", async function(event) {
+                if ((event.ctrlKey || event.metaKey) && event.key === "c") await navigator.clipboard.writeText(input.value);
             });
 
         });
@@ -35,4 +29,4 @@ window.addEventListener("load",()=>{
 
     },1000);
 
-})
+});
